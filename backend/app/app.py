af6beed4e6121eef445af43008b3179c59943e02
Pamela-guevara <pame18287@gmail.com>
2025-11-14 00:48:38 -0300

    cambio en el CORS

--- backend/app/app.py
+++ backend/app/app.py
@@ -9,7 +9,12 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "*"], # Ruta desde donde deberá arrancar el frontend
+        allow_origins=[
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "https://simplex-frontend.fly.dev",
+            "https://simplex-frontend.fly.dev/",
+        ], # Rutas desde donde deberá arrancar el frontend
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
